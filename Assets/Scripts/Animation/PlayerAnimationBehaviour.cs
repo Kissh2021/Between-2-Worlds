62c1fe8fce923b4036cbb9e418015c1112413898
Kissh Moll <kissh.horror@gmail.com>
2022-01-29 22:31:29 +0100

    :)

--- Assets/Scripts/Animation/PlayerAnimationBehaviour.cs
+++ Assets/Scripts/Animation/PlayerAnimationBehaviour.cs
@@ -7,17 +7,29 @@ public class PlayerAnimationBehaviour : MonoBehaviour
 {
     private Animator animator;
     private PlayerBehaviour pb;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
     
     
     void Start()
     {
         pb = GetComponent<PlayerBehaviour>();
         animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.normalized.x > 0)
+        {
+            sr.flipX = false;
+        }
+        else if (rb.velocity.normalized.x < 0)
+        {
+            sr.flipX = true;
+        }
+        animator.SetFloat("Velocity", rb.velocity.magnitude);
     }
 }
