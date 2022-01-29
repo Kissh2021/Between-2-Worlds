394d92fb7871e95f830d645d1c6dd46af1b77b3a
Kissh Moll <kissh.horror@gmail.com>
2022-01-29 22:43:56 +0100

    Update PlateformIntangibillity.cs

--- Assets/Scripts/PlateformIntangibillity.cs
+++ Assets/Scripts/PlateformIntangibillity.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlateformIntangibillity : MonoBehaviour
 {
@@ -10,12 +11,22 @@ public class PlateformIntangibillity : MonoBehaviour
     void Start()
     {
         m_playerT = FindObjectOfType<PlayerBehaviour>().transform;
-        
+        if (!TryGetComponent<Collider2D>(out m_collider))
+        {
+            m_collider = GetComponentInChildren<Collider2D>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Dot(Vector2.up, m_playerT.position - m_collider.transform.position) > 0)
+        {
+            m_collider.enabled = true;
+        }
+        else
+        {
+            m_collider.enabled = false;
+        }
     }
 }
