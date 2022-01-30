dc2ed53ddbe7bc16bf08d9fa039c6642a6564b7c
Kissh Moll <kissh.horror@gmail.com>
2022-01-30 03:51:35 +0100

    Player stuck killer and player stats adjustments

---
+++ Assets/Scripts/Player/PlayerStuckKiller.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Interfaces;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerStuckKiller : MonoBehaviour
+{
+    [SerializeField]
+    private float timeBeforeDie = 0.7f;
+
+    private float m_timer = 0f;
+    
+    private bool m_sometingInPlayer = false;
+
+    private PlayerBehaviour player;
+
+    private void Start()
+    {
+        player = GetComponent<PlayerBehaviour>();
+    }
+
+    void Update()
+    {
+        if (m_sometingInPlayer)
+        {
+            if (m_timer > timeBeforeDie)
+            {
+                (player as IDamageable).Hit();
+            }
+            m_timer += Time.deltaTime;
+        }
+        else
+        {
+            m_timer = 0f;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(player.transform.position, Vector2.up, 1.7f);
+
+        if (hits.Any(x => x.collider.gameObject != player.gameObject))
+        {
+            m_sometingInPlayer = true;
+        }
+        else
+        {
+            m_sometingInPlayer = false;
+        }
+    }
+}
